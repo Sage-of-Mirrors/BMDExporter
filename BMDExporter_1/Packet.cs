6fa54aad718cb3eee48602487edf9d2d1415c7bf
Sage-of-Mirrors <empressglatia@gmail.com>
2016-04-10 01:47:09 -0500

    Fixed winding order issue in a way that doesn't require the user to do anything.

--- BMDExporter_1/Packet.cs
+++ BMDExporter_1/Packet.cs
@@ -19,15 +19,32 @@ namespace BMDExporter_1
 
         public void AddTriangle(string[] source)
         {
+            // Since TWW is backwards when it comes to winding order, we're going to have to swap our vertexes.
+            // We start with a list of list<shorts> to hold our vertexes so we can ignore how the vertex is laid out.
+            List<List<short>> hah = new List<List<short>>();
+
+            // Process each vertex here. Each vert is a sequence of "pos/uv/normal" or something similar.
             for (int i = 1; i < 4; i++)
             {
+                List<short> blah = new List<short>();
+
+                // get pos, uv, normal, etc data where applicable
                 string[] indexes = source[i].Split('/');
 
+                // get each index and add to the local index list
                 for (int j = 0; j < indexes.Length; j++)
                 {
-                    m_triIndexes.Add((short)(Convert.ToInt16(indexes[j]) - 1));
+                    blah.Add((short)(Convert.ToInt16(indexes[j]) - 1));
                 }
+
+                // We're finished with this vertex, so add it to the mast list
+                hah.Add(blah);
             }
+
+            // To flip the winding order, we swizzle vertex 0 and 2.
+            m_triIndexes.AddRange(hah[2].ToArray());
+            m_triIndexes.AddRange(hah[1].ToArray());
+            m_triIndexes.AddRange(hah[0].ToArray());
 
             m_triCount++;
         }
